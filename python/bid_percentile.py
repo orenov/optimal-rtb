21edd13071c60ec5a0a1115cda896029003a31bc
orenov <forihcs@gmail.com>
2015-09-15 10:48:44 +0000

    perc

--- python/bid_percentile.py
+++ python/bid_percentile.py
@@ -6,11 +6,12 @@ from csv import DictReader
 if __name__ == '__main__':
     file_all_name = "../../make-ipinyou-data/all/train.log.txt"
     arr = []
-    with open(file_all_name, 'r') as f:
-        next(f)
-        for t, line in enumerate(DictReader(open(test), delimiter=',')):
-            arr.append(line['payprice'])
-
+        
+    for t, line in enumerate(DictReader(open(file_all_name), delimiter='\t')):
+        arr.append(line['payprice'])
+	if (t + 1) % 1000000 == 0:
+	    print("Proceeded {0} lines".format(t))
+    arr = np.array(arr, np.float)
     per = [1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 99]
     qua = np.percentile(arr, per)
 
