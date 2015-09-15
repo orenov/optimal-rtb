ccf3b9562401c0d274eced710431583192a31a81
orenov <orenov@codeminders.com>
2015-09-15 10:57:36 +0300

    stock coverage init

---
+++ python/stock_coverage.py
@@ -0,0 +1,18 @@
+import bz2
+
+if __name__ == '__main__':
+    bids_files1   = ["bid.20130311.txt.bz2", "bid.20130312.txt.bz2", "bid.20130313.txt.bz2", "bid.20130314.txt.bz2",/
+                     "bid.20130315.txt.bz2", "bid.20130316.txt.bz2", "bid.20130317.txt.bz2"]
+    imp_files1    = ["imp.20130311.txt.bz2", "imp.20130312.txt.bz2", "imp.20130313.txt.bz2", "imp.20130314.txt.bz2",/
+                     "imp.20130315.txt.bz2", "imp.20130316.txt.bz2", "imp.20130317.txt.bz2"]
+    folder_files1 = "training1st"
+
+    counter_bids = {}
+
+    for bid_file in bids_files1:    
+        source_file = bz2.BZ2File("../../make-ipinyou-data/original-data/ipinyou.contest.dataset/" + folder_files1 + "/" + bid_file, "r") 
+        for line in source_file: 
+            print line.strip("\t")
+        
+        print "Exiting" 
+        print "I used file: " + osm_file 
