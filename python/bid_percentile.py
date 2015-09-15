239ae1814d3e7cc9d351de4c58e11e5493075205
orenov <orenov@codeminders.com>
2015-09-15 13:55:03 +0300

    fs

--- python/bid_percentile.py
+++ python/bid_percentile.py
@@ -4,20 +4,22 @@ import numpy as np
 from csv import DictReader
 
 if __name__ == '__main__':
-    file_all_name = "../../make-ipinyou-data/all/train.log.txt"
+    if len(sys.argv) < 2:
+        print 'Usage: train.yzx.txt'
+        exit(-1)
+
+    file_all_name = "../../make-ipinyou-data/" + str(sys.argv[1]) + "/train.log.txt"
     arr = []
         
     for t, line in enumerate(DictReader(open(file_all_name), delimiter='\t')):
         arr.append(line['payprice'])
-	if (t + 1) % 1000000 == 0:
-	    print("Proceeded {0} lines".format(t))
     arr = np.array(arr, np.float)
     per = [1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 99]
     qua = np.percentile(arr, per)
 
-    print("--- Percentiles ---- ")
+    print("--- Percentiles For Campaign {0}---- ".format(sys.argv[1]))
     print(per)
-    print("--- Values ----")
+    print("--- Values For Campaign {0} ----".format(sys.argv[1]))
     print(qua)
 
 
